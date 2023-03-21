6fd5223e0e642fe188c56ea095a06a2d95aa377a
leticia <leticiaae@hotmail.com.br>
2023-03-21 16:05:01 -0300

    added express

--- server.js
+++ server.js
@@ -1,22 +1,8 @@
-const http = require("http")
-const port = 3000
+import App from "./src/App.js";
 
+const port = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Curso node',
-    '/livros':  'Entrei na página de livros',
-    '/autores': 'Listagem de autores',
-    '/editores': 'Pag de editora',
-    '/sobre' : 'Página de sobre',
-}
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-
-})
-
-
-server.listen(port, ()=> {
+App.listen(port, ()=> {
     console.log(`Servidor escutando em http://localhost:${port}`)
 }
 );
--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
-import  Express from "express";
+import express from "express";
+
+const App = express();
+
+const livros = [
+    {id:1,  "título": 'Senhor dos aneis'},
+    {id:2,  "título": 'O Hobiit'},
+]
+
+App.get('/', (req, res)=>{
+    res.status(200).send ("Curso de Node");
+
+})
+
+App.get('/livros', (req, res)=> {
+    res.status(200).json(livros)
+})
+
+export default App;
