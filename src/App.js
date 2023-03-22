5f8e9fc95f43ad287722d5a83e019e82ea1d1457
leticia <leticiaae@hotmail.com.br>
2023-03-22 11:08:20 -0300

    added post and put

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import express from "express";
 
 const App = express();
-
+App.use(express.json());
 const livros = [
-    {id:1,  "título": 'Senhor dos aneis'},
-    {id:2,  "título": 'O Hobiit'},
-]
+  { id: 1, título: "Senhor dos aneis" },
+  { id: 2, título: "O Hobiit" },
+];
 
-App.get('/', (req, res)=>{
-    res.status(200).send ("Curso de Node");
+App.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
+});
 
-})
+App.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
 
-App.get('/livros', (req, res)=> {
-    res.status(200).json(livros)
-})
+App.get("/livros/:id", (req, res) => {
+  const result = livros[req.params.id];
+  if (result) res.status(200).json(result);
+  else
+    res.status(404).json({
+      error: true,
+      message: "Livro não encontrado",
+    });
+});
 
-App.post('/livros', (req, res)=>{
+App.post("/livros", (req, res) => {
+  livros.push({
+    id: livros.length + 1,
+    ...req.body,
+  });
 
-    livros.push(req.body)
-    res.status(201).send("Livro foi cadastrado com sucesso!")
-})
+  res.status(201).send("Livro foi cadastrado com sucesso!");
+});
 
+App.put("/livros/:id", (req, res) => {
+  let { livro, position} = buscaLivro(req.params.id);
+  livro = {
+    id: livro.id,
+    ...req.body,
+  };
+  livros[position] = livro;
+  res.status(200).json(livros[position]);
+});
+
+function buscaLivro(id) {
+  const livro = livros.find((livros) => livros.id == id);
+  const position = livros.findIndex((livros) => livros.id == id);
+  return {
+    livro: livro,
+    position: position,
+  };
+}
 export default App;
