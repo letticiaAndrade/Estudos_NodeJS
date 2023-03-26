394d2a5f8a179886c8e912393d561b56cf2c9c85
leticia <leticiaae@hotmail.com.br>
2023-03-26 18:14:35 -0300

    added correct code

--- src/models/Livros.js
+++ src/models/Livros.js
@@ -4,12 +4,12 @@ import mongoose from "mongoose";
 // um schema, modelo de atributos
 const livroSchema = new mongoose.Schema(
     {
-    id: {type:String}, 
-    titulo: {type:String, require: true},
-    autor: {type:String, require: true},
-    editora: {type:String, require: true},
-    numeroPaginas: {type: Number},
-}
+        id: { type: String },
+        titulo: { type: String, required: true },
+        autor: { type: String, required: true },
+        editora: { type: String, required: true },
+        numeroPaginas: { type: Number },
+    }
 )
 
 // criar uma variavel para permitir que seja exportada em outro lugar e ser usada
@@ -17,7 +17,7 @@ const livroSchema = new mongoose.Schema(
 
 // criar uma coleção chamada livros 
 // const livros= mongoose.model('livros');
-const livros= mongoose.model('livros', livroSchema);
+const livros = mongoose.model('livros', livroSchema);
 
 export default livros;
 
