a8ab03ba641ebf8470c0486655aca167c418f4db
leticia <leticiaae@hotmail.com.br>
2023-03-26 18:15:59 -0300

    added put method

--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -4,6 +4,7 @@ import LivroController from "../controllers/livroController.js";
 const router = express.Router();
 
 router
-    .get("/livros", LivroController.listarLivros);
-
+    // se houver uma requisição do tipo get em livros faça isso
+    .get("/livros", LivroController.listarLivros)
+    .post("/livros", LivroController.cadastrarLivro)
 export default router;
